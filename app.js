ba6b2f537e4958802bc87b8657778897aafbc4bb
Rod McNew <rmcnew@relivinc.com>
2015-04-18 11:15:49 -0500

    initial commit

--- app.js
+++ app.js
@@ -13,19 +13,55 @@ var maxY = height - 1;
 var springStiffness = .1;
 
 //The Z's measure how much the field is distorted
-var zs = [];
-forEachRow(function (y) {
-    zs[y] = [];
-});
-forEachPoint(function (y, x) {
-    zs[y][x] = 0;
-});
+var zs, velocities, forces;
+
+
+function tick() {
+    logzs();
+    forEachPoint(calculateForce);
+    forEachPoint(applyForce);
+}
+
+function calculateForce(y, x) {
+    var top = 0, right = 0, bottom = 0, left = 0;
+    var z = zs[y][x];
+    if (y > 0) {
+        top = z - zs[y - 1][x];
+    }
+    if (y < maxY) {
+        bottom = z - zs[y + 1][x];
+    }
+    if (x < 0) {
+        left = z - zs[y][x - 1];
+    }
+    if (x < maxX) {
+        right = z - zs[y][x + 1];
+    }
+    //Divide by 2 to share the force across the spring
+    forces[y][x] = (top + right + bottom + left) / 2 * springStiffness
+}
+
+function applyForce(y, x) {
+    velocities[y][x] += forces[y][x];
+    zs[y][x] += velocities[y][x];
+    zs[y][x] = Math.round(zs[y][x]);
+}
+
+function init() {
+    zs = [];
+    forEachRow(function (y) {
+        zs[y] = [];
+    });
+    forEachPoint(function (y, x) {
+        zs[y][x] = 0;
+    });
 
 //Create 0-filled matrixes
-var velocities = zs;
-var forces = zs;
+    velocities = zs;
+    forces = zs;
 
-zs[20][0] = 100;
+    zs[20][0] = 100;
+}
 
 function forEachPoint(callback) {
     for (var y = 0; y < height; y++) {
@@ -41,38 +77,6 @@ function forEachRow(callback) {
     }
 }
 
-function calculateForce(y, x) {
-    var top = 0, right = 0, bottom = 0, left = 0;
-    var z = zs[y][x];
-    if (y > 0) {
-        top = z - zs[y - 1][x];
-    }
-    if (y < maxY) {
-        bottom = z - zs[y + 1][x];
-    }
-//    if (x < 0) {
-//        left = z - zs[y][x - 1];
-//    }
-//    if (x < maxX) {
-//        right = z - zs[y][x + 1];
-//    }
-//    console.log(top,right,bottom,left);
-    //Divide by 2 to share the force across the spring
-    forces[y][x] = (top + right + bottom + left) / 2 * springStiffness
-}
-
-function applyForce(y, x) {
-    velocities[y][x] += forces[y][x];
-    zs[y][x] += velocities[y][x];
-    zs[y][x] = Math.round(zs[y][x]);
-}
-
-function tick() {
-    logzs();
-    forEachPoint(calculateForce);
-    forEachPoint(applyForce);
-}
-
 function logzs() {
     console.log();
     for (var y = 0; y < height; y++) {
@@ -84,6 +88,7 @@ function logzs() {
     }
 }
 
+init();
 setInterval(tick, 1000);
 
 //http.createServer(app).listen(app.get('port'));
